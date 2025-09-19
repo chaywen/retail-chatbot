b0903effa80f74f858fe1d8dd16ecb32c60d45b1
chaywen <lchaywen06@gmail.com>
2025-09-19 23:07:42 +0800

    Update ChatWindow.jsx

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -13,7 +13,7 @@ export default function ChatWindow() {
   };
 
   return (
-    <div className="relative flex flex-col min-h-screen bg-white overflow-hidden font-sans">
+    <div className="relative flex flex-col h-screen bg-white overflow-hidden font-sans">
       {/* Radial layers */}
       <div className="radial-layer w-96 h-96 top-10 left-20 animate-[pulse-radial_8s_ease-in-out_infinite]"></div>
       <div className="radial-layer w-72 h-72 top-40 right-10 animate-[pulse-radial_10s_ease-in-out_infinite]"></div>
