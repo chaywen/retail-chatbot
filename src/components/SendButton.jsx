9f932ddf5aa5c63defd17d505fa316a09c68ad6e
chaywen <lchaywen06@gmail.com>
2025-09-20 10:01:09 +0800

    Update SendButton.jsx

--- src/components/SendButton.jsx
+++ src/components/SendButton.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { FiSend } from 'react-icons/fi';
+import './input.css';
 
+export const SendButton = ({ onClick, disabled }) => {
+  return (
+    <button
+      className={`send-button ${disabled ? 'disabled' : ''}`}
+      onClick={onClick}
+      disabled={disabled}
+      aria-label="Send"
+    >
+      <FiSend size={20} />
+    </button>
+  );
+};
